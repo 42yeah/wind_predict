aa3aa72f8fdadd9b90cd3f8547d986141bdf6e6a
42yeah <42yeah@gmx.fr>
2021-08-22 17:28:55 +0800

    Pretty good. GitHubbing.

--- net.py
+++ net.py
@@ -28,9 +28,10 @@ class Net:
             x = sigmoid(np.matmul(w, x) + b)
         return x
 
-    def stochastic_gradient_descent(self, epoch, training_data, mini_batch_size, eta, test_data,
+    def stochastic_gradient_descent(self, epoch, training_data, mini_batch_size, eta, lmbda, test_data,
                                     monitor_evaluation_cost=False,
-                                    render_evaluation=False):
+                                    render_evaluation=False,
+                                    monitor_evaluation_rmse=False):
         for i in range(epoch):
             print(f"Commencing epoch {i}...")
             random.shuffle(training_data)
@@ -39,11 +40,13 @@ class Net:
                 for k in range(0, len(training_data), mini_batch_size)
             ]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                self.update_mini_batch(mini_batch, eta, lmbda, len(training_data))
 
             print(f"Epoch {i} complete. ", end="")
-            if monitor_evaluation_cost or render_evaluation:
+            if monitor_evaluation_cost or render_evaluation or \
+                    monitor_evaluation_rmse:
                 total_cost = 0
+                rmse = 0
                 predicted = []
                 expected = []
                 # test_data = sorted(test_data, key=lambda x: x[1])
@@ -52,18 +55,22 @@ class Net:
                     if len(predicted) < 100:
                         predicted.append(a[0][0])
                         expected.append(y[0][0])
-                    total_cost += self.cost_func.cost(a, y, 1)
+                    rmse += np.power(np.sum(a - y), 2)
+                    total_cost += self.cost_func.cost(a, y, len(test_data))
+                rmse = np.sqrt(rmse / len(test_data))
                 if monitor_evaluation_cost:
-                    print(f"Evaluation data cost: {total_cost}", end="")
+                    print(f"Evaluation data cost: {total_cost}", end=", ")
+                if monitor_evaluation_rmse:
+                    print(f"Evaluation RMSE: {rmse}", end=", ")
                 if render_evaluation:
                     fig = plt.figure()
                     ax = plt.axes()
                     ax.plot(predicted, color="b")
                     ax.plot(expected, color="r")
                     plt.show()
-            print()
+            print("done.")
 
-    def update_mini_batch(self, mini_batch, eta):
+    def update_mini_batch(self, mini_batch, eta, lmbda, n):
         nabla_w = [np.zeros(x.shape) for x in self.weights]
         nabla_b = [np.zeros(x.shape) for x in self.biases]
         for x, y in mini_batch:
